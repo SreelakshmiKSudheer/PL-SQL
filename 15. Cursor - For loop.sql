3b11dd5d5b16924d65a955d5ac787c0848aa48e7
SreelakshmiKSudheer <sreelakshmikavenge3i3@gmail.com>
2024-11-14 15:22:26 +0530

    cursor with for loop

---
+++ 15. Cursor - For loop.sql
@@ -0,0 +1,36 @@
+SET SERVEROUTPUT ON;
+
+CREATE TABLE customer (
+    id INT,
+    name VARCHAR2(20),
+    dob DATE,
+    mobile NUMBER,
+    city VARCHAR2(20)
+);
+
+INSERT INTO customer VALUES (1, 'Kakashi', DATE '1998-09-15', 7894651230, 'konoha');
+INSERT INTO customer VALUES (2, 'Naruto', DATE '2002-10-10', 7896451230, 'konoha');
+INSERT INTO customer VALUES (3, 'Sasuke', DATE '2001-05-05', 7894561230, 'konoha');
+INSERT INTO customer VALUES (4, 'Sakura', DATE '2001-02-15', 8794651230, 'konoha');
+
+SELECT * FROM customer;
+
+DECLARE
+    eName customer.name%type;
+    cursor emp
+    is select name into eName from customer;
+    n number;
+
+begin 
+    for rec in emp loop
+        n := emp%rowcount;
+        dbms_output.put_line(rec.name);
+    end loop;        
+    
+    dbms_output.put_line(n);
+end;
+/
+
+drop table customer;
+
+-- @ "C:\Users\user\OneDrive\Documents\Courses\PL SQL\15. Cursor - For loop.sql"
